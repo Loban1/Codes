f3e5a6db7bc04d7f33b2137f48f890505f2a63dd
João S.C <lobanturing@gmail.com>
2026-04-30 19:19:07 -0400

    Fix Fibonacci function and update output index

--- Fibonacci.c
+++ Fibonacci.c
@@ -7,17 +7,14 @@ int main()
 {
     for(int i=1; i<=100; i++)
     {
-        printf("Fibonacci Nº%d: %lld\n", i-1, fibonacci(i));
+        printf("Fibonacci Nº%d: %lld\n", i, fibonacci(i));
     }
     return (0);
 }
 
 long long int fibonacci(int i)
 {
-    if(i == 0 || i == 1) 
-    {
-        vetor[i] = i; return i;
-    }
+    if(i == 0 || i == 1) return 1;
     vetor[i] = fibonacci(i-1);
     return vetor[i]+vetor[i-1];
 }
